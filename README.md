ea4ff3ebb7ded7af36c746541b9f500e81e0aaee
Christian <christian.mortaro@gmail.com>
2020-11-25 10:00:09 -0300

    :tada: :sparkles: initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<img src='https://raw.githubusercontent.com/nullstack/nullstack/master/nullstack.png' height='60' alt='Nullstack'>
+
+# Create Nullstatic App
+
+Crawls every accessible link in your application and generates static HTML.
+
+## Usage
+
+```bash
+npx create-nullstatic-app
+```
+
+The default output folder is "static". 
+
+To select a new folder run:
+
+```bash
+npx create-nullstatic-app folder-name
+```
+
+Refer to the [Nullstack Repository](https://github.com/nullstack/nullstack) for more information.
+
+## License
+
+create-nullstatic-app is released under the [MIT License](https://opensource.org/licenses/MIT).
---
+++ index.js
@@ -0,0 +1,69 @@
+#! /usr/bin/env node
+
+const folder = process.argv[2] || 'static';
+
+const reserved = ['public', '.production', '.development', 'src'];
+
+if(reserved.includes(folder)) {
+  console.log('\x1b[31m%s\x1b[0m', `You cannot use a folder named "${folder}"`);
+  process.exit();
+}
+
+const fetch = require('node-fetch');
+const links = {};
+const {existsSync, mkdirSync, writeFileSync, copySync, readdirSync, rmdirSync} = require('fs-extra');
+const {execSync, fork} = require('child_process');
+
+async function crawl(port, path) {
+  
+  links[path] = true;
+  const response = await fetch('http://localhost:' + port + path);
+  const html = await response.text();
+
+  if(!existsSync(folder + path)) {
+    mkdirSync(folder + path, {recursive: true});
+  }
+
+  writeFileSync(folder + path + '/index.html', html);
+
+  const pattern = /<a href="(.*?)"/g;
+  while(match=pattern.exec(html)){
+    const link = match[1];
+    if(link.startsWith('/')) {
+      if(links[link] === undefined) {
+        links[link] = false;
+      }
+    }
+  }
+
+  for(const link in links) {
+    if(!links[link]) {
+      await crawl(port, link);
+    }
+  }
+  
+};
+
+async function run() {
+  rmdirSync(folder, {recursive: true});
+  await execSync('npm run build');
+  const server = fork('.production/server.js', [], {silent: true});
+  server.stdout.on('data', async (buffer) => {
+    const lookup = 'production mode at http://127.0.0.1:';
+    const message = buffer.toString();
+    if(message.indexOf(lookup) > -1) {
+      const port = parseInt(message.split(lookup)[1]);
+      await crawl(port, '/');
+      server.kill();
+      copySync('public', folder);
+      for(const file of readdirSync('.production')) {
+        if(file.startsWith('client')) {
+          copySync('.production/' + file, folder + '/' + file);
+        }
+      }
+      console.log('\x1b[36m%s\x1b[0m', 'Yay! Your static Nullstack application is ready.');
+    }
+  });  
+}
+
+run();
